98487bed52f34bdfb88a70040813b453cb3a1dfd
Ayush Upneja <upneja@bu.edu>
2019-07-15 22:23:59 -0400

    Add files via upload

---
+++ Hackerrank Minimum Absolute Difference in an Array.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    n = len(arr)
+    arr = sorted(arr)
+    r = []
+    for i in range(n-1):
+        r.append(abs(arr[i]-arr[i+1]))
+    return min(r)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
